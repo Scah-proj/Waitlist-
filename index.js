6a27bac99dd715f2675dd81d5440364977b0458c
Ayomide Aderotoye <ayomide.aderotoye@gmail.com>
2025-10-11 00:45:38 +0100

    waitlist

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-document.getElementById("waitlist-form").addEventListener("submit", async function(e) {
+const form = document.getElementById("waitlist-form");
+const message = document.getElementById("message");
+form.addEventListener("submit", async (e) => {
     e.preventDefault();
 
     const role = document.getElementById("role").value;
+    
     const email = document.getElementById("email").value;
+
 
     if (!role || !email) {
       alert("Please select a role and enter your email.");
@@ -10,7 +14,7 @@ document.getElementById("waitlist-form").addEventListener("submit", async functi
     }
 
     try {
-      const res = await fetch("", {
+      const res = await fetch("https://scahwaitist.onrender.com/api/waitlist", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
@@ -19,7 +23,13 @@ document.getElementById("waitlist-form").addEventListener("submit", async functi
       });
 
       const data = await res.json();
-      alert("Success! " + data.message);
+       if (res.ok) {
+      message.textContent = " Success! " + data.message;
+      message.style.color = "green";
+    } else {
+      message.textContent = " Error: " + (data.message || "Something went wrong");
+      message.style.color = "red";
+    }
     } catch (err) {
       console.error(err);
       alert(" Something went wrong. Try again.");
